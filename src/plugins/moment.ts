3e796cafe2aa130246f1b54d4ef216ca8021e917
ansoxlu <anso.xlu@gmail.com>
2022-02-13 00:53:10 +0800

    Updates

--- src/plugins/moment.ts
+++
@@ -1,3 +0,0 @@
-export const DATE = 'YYYY-MM-DD'
-export const TIME = 'hh:mm:ss'
-export const DATE_TIME = `${DATE} ${TIME}`
--- src/template/editor/types.ts
+++
@@ -1,9 +0,0 @@
-export enum DroppableIds {
-  Building = 'Building',
-  Material = 'Material',
-}
-
-export enum MaterialType {
-  Layout = 'layout',
-  Content = 'content',
-}
