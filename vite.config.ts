8246cf88d80c7ee962fd197f981458bd21f36556
ansoxlu <anso.xlu@gmail.com>
2022-03-30 01:16:45 +0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// @ts-ignore
+import config from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${config.name}` : undefined,
   plugins: [react()],
-});
+})
